fbe2f5028f75ec11f15246b190f066da4f312680
oissor <oissor.min@gmail.com>
2019-10-30 11:47:31 -0400

    web inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,160 @@
+<!DOCTYPE HTML>
+<!--
+	Stellar by HTML5 UP
+
+	html5up.net | @ajlkn
+        Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+    -->
+<html>
+<head>
+    <title>UATF::Vicerrectorado</title>
+    <meta charset="utf-8" />
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+    <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+</head>
+<body>
+
+<!-- Wrapper -->
+<div id="wrapper">
+
+    <!-- Header -->
+    <header id="header" class="alt">
+        <span class="logo"><img src="images/logos.png" alt="" /></span>
+        <h1>Vicerrectorado UATF</h1>
+        <p>Excelencia académica 2017<br />
+    </header>
+
+    <!-- Nav -->
+    <nav id="nav">
+        <ul>
+            <li><a href="#intro" class="active">Vicerrectorado</a></li>
+            <li><a href="#cta">Plataforma Virtual</a></li>
+            <li><a href="#descargas">Descargas</a></li>
+
+        </ul>
+    </nav>
+
+
+    <!-- Main -->
+    <div id="main">
+
+        <!-- Introduction -->
+        <section id="intro" class="main">
+            <div class="spotlight">
+                <div class="content">
+                    <header class="major">
+                        <h2>Vicerrectorado UATF</h2>
+                    </header>
+                    <p>El vicerrectorado de la UATF es la máxima instancia académica,
+                        que administra el aspecto académico de las Facultades,
+                        carreras y programas.
+                        De ella dependen las direcciones intermedias como son:
+                        Dirección de Servicios Académicos,
+                        Dirección de Investigación Científica y Tecnológica,
+                        Dirección de Extensión Universitaria
+                        y la Dirección de Postgrado. <br>
+                        Actualmente el Sr. Vicerrector titular, electo por el
+                        Co-Gobierno de la UATF es el Dr. Ing. Pedro Guido López Cortés,
+                        quien asumió sus funciones desde el 2014 al 2018 a quien se le
+                        atribuye el trabajo presentado con fundamentos científicos
+                        y con el uso de instrumentos estadísticos determino que el
+                        problema principal de la UATF es el bajo rendimiento académico
+                        que lleva a una permanencia ilimitada del estudiante
+                        en su profesionalización.</p>
+
+                </div>
+                <span class="image"><img src="images/vicerrector.jpg" alt="" /></span>
+            </div>
+        </section>
+
+
+        <!-- Get Started -->
+        <ul class="actions">
+            <li class="center">
+                <a href="http://190.129.25.124/test_uatf" class="button special"><span class="icon fa-laptop"></span> TEST DE ORIENTACION VOCACIONAL</a>
+            </li>
+        </ul>
+
+
+        <section id="cta" class="main special">
+            <header class="major">
+                <h2>Educación Virtual Combinada <br>en Clase (Presencial - Virtual)</h2>
+                <p>Esta plataforma esta basada en una estratégia didáctica Blended Learning para mejorar el PEA, en Grado y Posgrado, implementado en el campus Universitario de la UATF.<br /></p>
+            </header>
+            <footer class="major">
+
+
+                <ul class="actions">
+                    <li>
+                        <a href="http://190.129.25.124/edidacticabl3" class="button special"><span class="icon fa-laptop"></span> PLATAFORMA VIRTUAL</a>
+                    </li>
+                </ul>
+                CURSOS REALIZADOS<br>
+                - Maestria en Gineco Ostetrícia...
+            </footer>
+        </section>
+
+        <!-- Get Started -->
+        <section id="descargas" class="main special">
+            <header class="major">
+                <h2>Descargas</h2>
+                <p>Documentos, requisitos, etc...</p>
+            </header>
+            <ul class="statistics">
+
+                <li class="style2">
+                    <a href="{!! url('/pdf/doc/modv7_extraordinario.pdf') !!}">
+                        <span class="icon fa-folder-open-o"></span>
+                        <strong>Requisitos</strong>Docente Extraordinario
+                    </a>
+                </li>
+                <li class="style3">
+                    <a href="{!! url('/pdf/doc/modv7_ordinarios.pdf') !!}">
+                        <span class="icon fa-folder-open-o"></span>
+                        <strong>Requisitos</strong> Docente Ordinario
+                    </a>
+                </li>
+            </ul>
+        </section>
+
+    </div>
+
+    <!-- Footer -->
+    <footer id="footer">
+        <section>
+            <h2>Redes Sociales</h2>
+            <ul class="icons">
+                <li><a href="https://www.facebook.com/Vicerrectorado-UATF-1819324954951130/?fref=ts" class="icon fa-facebook alt"><span class="label">Facebook</span></a></li>
+            </ul>
+        </section>
+
+
+        <section>
+            <h2>Contactos</h2>
+            <dl class="alt">
+                <dt>Dirección</dt>
+                <dd>Av. Civica esq. Serrudo S/n &bull; Potosí - Bolivia</dd>
+                <dt>Teléfono</dt>
+                <dd>(591) 2 6227329</dd>
+            </dl>
+        </section>
+        <p class="copyright">&copy; Design: Webvice 2017</p>
+    </footer>
+</div>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.scrollex.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/skel.min.js"></script>
+<script src="assets/js/util.js"></script>
+<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
